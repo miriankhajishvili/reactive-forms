fdacd242f2b74911e5e5dbf9bb6b6d09cf6eeb2d
mirian.khajishvili@gmail.com <mirian.khajishvili@gmail.com>
2022-12-01 23:08:49 +0400

    add reactive-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'reactive-forms';
+
+  get userName(){
+
+    return this.registrationForm.get('usarName')
+  }
+
+
+constructor(private fb: FormBuilder){}
+
+registrationForm: any= this.fb.group({
+  userName: ['',[ Validators.required, Validators.minLength(3)]],
+  password: [''],
+  confirmPassword: [''],
+  address: this.fb.group({
+    city: [''],
+    state: [''],
+    postalCode: ['']
+    
+
+ })
+  
+})
+
+
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Vishwas'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+
+
+  // });
+
+  loadApiData(){
+    this.registrationForm.patchValue({
+      userName: '',
+      password: '',
+      confirmPassword: '',
+      // address: {
+      //   city: 'City',
+      //   state: 'State',
+      //   postalCode: '123456'
+      // }
+    })
+  }
 }
